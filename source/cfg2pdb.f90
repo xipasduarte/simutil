a90b658ec6cfc0bebb545e4be3d0835015f14e51
Pedro Duarte <xipasduarte@gmail.com>
2014-05-23 14:18:45 +0100

    cfg2pdb correct to yield no EOF error.
    Also added correction to the atom identifiers.

--- source/cfg2pdb.f90
+++ source/cfg2pdb.f90
@@ -2,6 +2,7 @@
 ! 
 ! Revised on: May 1, 2014
 ! Author: Zé Nuno
+! Revision Author: Pedro Duarte
 ! License: MIT License
 ! Description: Converts a DL_POLY configuration file 
 ! into a rasmol readable pdb file.
@@ -10,37 +11,40 @@ program cfg2pdb
 
 	implicit none
 
-	character header*80
-	character atmnam*8, end*3
-	real(8) xxx, yyy, zzz
-	integer nmol, levcnf, imcon
+	character :: header*80, atmnam*4, fmt_header*20
+	real :: x, y, z
+	integer :: nmol, levcnf, imcon, status, i
 
-	open (2, file = 'CONFIG') ; open (3, file = 'RASMOL')
+	open(2, file='CONFIG', status="old", action="read")
+	open(3, file='RASMOL', status="replace", action="write")
 
+	! Variable initialization
+	fmt_header = '(A80,/,2(I10))'
 
+	read(2,fmt_header) header, levcnf, imcon
+	write(3,*) header
+	
+	read(2,'(2/)')
+	
+	do
+		read (2,*,iostat=status) atmnam, nmol
+		
+		if(status .ne. 0) then
+			EXIT
+		end if
+		read(2,'(3F20.0)') x, y, z
 
-	read (2,'(A80,/,2(i10))') header, levcnf, imcon
-	if (imcon.gt.0) read (2,'(2(/))')
-	write (3,*) header ; write (3,*) levcnf, imcon
-
-
-
-	nmol = 1
-
-        read (2,'(A8)') atmnam
-
-	do while (atmnam.ne."END     ")
-
-		read (2,'(3(f20.0))') xxx, yyy, zzz
-
-		if (levcnf.gt.0) read (2,'(/)')
-
-		write (3,'(A6,I5,2X,A3,14X,3F8.3, I3)') &
-		'ATOM  ', nmol, atmnam, xxx, yyy, zzz
-		nmol = nmol + 1
-
-                read (2,'(A8)') atmnam
-
+		if(len_trim(atmnam)<4) then
+			write(3,'(A6,I5,2X,A3,14X,3F8.3,23X,A1)') &
+			'ATOM  ', nmol, atmnam, x, y, z, atmnam
+		else
+			write(3,'(A6,I5,1X,A4,14X,3F8.3,23X,A1)') &
+			'ATOM  ', nmol, atmnam, x, y, z, atmnam
+		end if
+		
+        do i=1,levcnf
+			read(2,*)
+		end do
 	end do
 
 	close (2) ; close (3)
