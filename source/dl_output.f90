f326c7ba4612e95085a813305948d9c06ea7e622
xipasduarte <pedro.duarte@tecnico.ulisboa.com>
2014-11-26 17:36:40 +0000

    dl_output
    Supply a list of rolling averages and Statistical Inefficiency for a set
    of block sizes.

--- source/dl_output.f90
+++ source/dl_output.f90
@@ -10,12 +10,12 @@ program dl_output
 	implicit none
 	! Variable declaration
 	! Allocatable
-	real, allocatable :: values(:)
-	integer, allocatable :: keys_arr(:), temp_i(:)
+	real, allocatable :: values(:), b_stdev(:,:,:), b_var(:,:), b_s(:,:)
+	integer, allocatable :: keys_arr(:), temp_i(:), b_info(:,:), temp_ij(:,:)
 	! Allocated
 	character :: arg*20, label_list(27)*12, keys*70, srcfile*80="OUTPUT", outfile*80="OUTPUT-dl_output", line*80,&
 	label*9
-	integer :: status, var, narg, i, j, k, ts_tot=0, ts, prop_size, stack=1, label_check, skiper=1
+	integer :: status, var, narg, i, j, k, ts_tot=0, ts, prop_size, stack=1, label_check, skiper=1, tp, kb
 	logical :: col=.false.
 	real :: mean(2,27)
 	
@@ -59,7 +59,7 @@ program dl_output
 	! Write properties to choose from on screen and get the user's selection
 	if(.not. col) then
 		do i=1,size(label_list, 1)
-			if(mod(i,5)==0) then
+			if(mod(i,9)==0) then
 				write(*,"(i2,a3,a12)", advance="yes") i, " - ", adjustl(label_list(i))
 			else
 				write(*,"(i2,a3,a12)", advance="no") i, " - ", adjustl(label_list(i))
@@ -93,16 +93,45 @@ program dl_output
 			call move_alloc(temp_i, keys_arr)
 		end if
 	end do
+	write(*,*) keys, keys_arr
 	
 	! Open files
 	open(1, file=srcfile, status="old", action="read") ! Open OUTPUT
 	open(2, file=outfile, status="replace", action="write") ! Open output file
 	
+	
 	! Transcribe header & search for data stacking interval
 	read(1,"(9(/),a80,13(/),30X,i12,8(/),30X,i12)") line, ts_tot, stack; ts = stack;
 	write(2,*) adjustl(line)
 	
+	
 	! Determine the size for the STDEV array
+	tp = ts_tot/stack; kb = tp
+	do while(kb > 1)
+		do i=2,7
+			if(mod(kb,i)==0) then
+				kb = kb/i
+				
+				! Increment b_info
+				if(allocated(b_info)) then
+					allocate(temp_ij(3,size(b_info,2)+1))
+					temp_ij(:,1:size(b_info,2)) = b_info
+				else
+					allocate(temp_ij(3,1))
+				end if
+				call move_alloc(temp_ij, b_info)
+				
+				! Add values to b_info
+				b_info(1,size(b_info,2)) = tp/kb
+				b_info(2,size(b_info,2)) = kb
+				b_info(3,size(b_info,2)) = kb*stack
+
+				exit
+			end if
+		end do
+	end do
+	allocate(b_stdev(size(keys_arr,1),tp/b_info(2,size(b_info,2)),size(b_info,2)))
+	b_stdev(:,:,:) = 0.0
 	
 	
 	! Write labels
@@ -119,6 +148,7 @@ program dl_output
 	else
 		write(2,"(i12)", advance="yes") keys_arr(size(keys_arr,1))
 	end if
+	
 	
 	! Process each timestep
 	do while(ts_tot>=ts)
@@ -139,9 +169,8 @@ program dl_output
 			end if
 			
 			if (label_check == ts) then
-				! Write and update ts and update skiper
+				! Write and update skiper
 				write(2,"(i9,2X)", advance="no") ts
-				ts = ts + stack
 				skiper = skiper + 1
 				
 				! Jump step values to averages
@@ -151,27 +180,39 @@ program dl_output
 				do i=1,3
 					do j=1,9
 						if(j==1) then
-							read(1,"(9X,e12.4)", advance="no") values((i-1)*3+j)
+							read(1,"(9X,e12.4)", advance="no") values((i-1)*9+j)
 						else
-							read(1,"(e12.4)", advance="no") values((i-1)*3+j)
+							read(1,"(e12.4)", advance="no") values((i-1)*9+j)
 						end if
 					end do
 					read(1,*)
 				end do
 		
 				! Select requested values and record in output file and array
-				do i=1,size(keys_arr,1)
-					if(i==size(keys_arr,1)) then
-						write(2,"(e12.4)", advance="yes") values(keys_arr(i))
-					else
-						write(2,"(e12.4)", advance="no") values(keys_arr(i))
-					end if
+				do k=1,size(keys_arr,1)
+					write(2,"(e12.4)", advance="no") values(keys_arr(k))
+					
+					! Increment statistics
+					do j=1,size(b_info,2)
+						if(mod(ts/stack,b_info(2,j))==0) then
+							i = ts/stack/b_info(2,j)
+						else
+							i = (ts/stack-mod(ts/stack,b_info(2,j)))/b_info(2,j)+1
+						end if
+						!write(*,*) i, ts, stack, b_info(2,j)
+						b_stdev(k,i,j) = b_stdev(k,i,j) + values(keys_arr(k))
+					end do
 				end do
-				deallocate(values)
+				write(2,*) ! Go to next line
 				
+				! Update ts
+				ts = ts + stack
+				
+				deallocate(values)
 			end if
 		end if
 	end do
+	
 	
 	! Get Averages and R.M.S. fluctuations
 	read(1,"(16/)") ! skip lines
@@ -179,9 +220,9 @@ program dl_output
 		do i=1,3
 			do j=1,9
 				if(j==1) then
-					read(1,"(9X,e12.4)", advance="no") mean(k,(i-1)*3+j)
+					read(1,"(9X,e12.4)", advance="no") mean(k,(i-1)*9+j)
 				else
-					read(1,"(e12.4)", advance="no") mean(k,(i-1)*3+j)
+					read(1,"(e12.4)", advance="no") mean(k,(i-1)*9+j)
 				end if
 			end do
 			read(1,*)
@@ -189,14 +230,46 @@ program dl_output
 		read(1,*)
 	end do
 
+
 	! Write averages
 	write(2,"(/,a9,2X)", advance="no") "Averages"
 	do i=1,size(keys_arr,1)
-		if(i==size(keys_arr,1)) then
-			write(2,"(e12.4)", advance="yes") mean(1,keys_arr(i))
-		else
-			write(2,"(e12.4)", advance="no") mean(1,keys_arr(i))
-		end if
+		write(2,"(e12.4)", advance="no") mean(1,keys_arr(i))
+	end do
+	write(2,"(/,a9,2X)", advance="no") "RMS flu."
+	do i=1,size(keys_arr,1)
+		write(2,"(e12.4)", advance="no") mean(2,keys_arr(i))
+	end do
+	
+	
+	! Statistics - calculate STDEV
+	allocate(b_var(size(keys_arr,1),size(b_info,2))); b_var(:,:) = 0.0;
+	allocate(b_s(size(keys_arr,1),size(b_info,2)))
+	do k=1,size(b_stdev,1)
+		do j=1,size(b_stdev,3)
+			do i=1,size(b_stdev,2)
+				if(b_stdev(k,i,j) /= 0) then
+					b_var(k,j) = b_var(k,j) + (b_stdev(k,i,j)/b_info(2,j)-mean(1,keys_arr(k)))**2/b_info(1,j)
+				end if
+			end do
+			b_s(k,j) = b_info(3,j)*b_var(k,j)/(mean(2,keys_arr(k))**2*ts_tot)
+		end do
+	end do
+	write(2,"(3/,a22,/)") "-- Block Statistics --"
+	
+	write(2,"(a12)",advance="no") "Block size  "
+	do j=1,size(b_info,2)
+		write(2,"(i12)",advance="no") b_info(2,j)*stack
+	end do
+	write(2,*)
+	
+	write(2,"(/,a28)") "Statistical Inefficiency (s)"
+	do k=1,size(b_s,1)
+		write(2,"(a12)",advance="no") adjustl(label_list(keys_arr(k)))
+		do j=1,size(b_s,2)
+			write(2,"(e12.4)",advance="no") b_s(k,j)
+		end do
+		write(2,*)
 	end do
 	
 	close(1); close(2)			
